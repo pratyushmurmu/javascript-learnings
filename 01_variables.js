ca5a27c0ee01b56e11b24a6bbe65fed80e84d974
pratyush <pratyushmurmu2004@gmail.com>
2025-12-23 21:33:03 +0530

    Initial JavaScript Practice

---
+++ 01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 122364
+let accountEmail = "pratyushmurmu@gmail.com"
+var accountPassword = "123456"
+accountCity = "Baripada"
+let accountState;
+//accountId = 9// not allowed
+accountEmail = "patyushmumu@gmail.com"
+accountPassword = "24689"
+accountCity = "dhenkanal"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+let x = 2;
+
+let y = x*x;
+
+console.log(y);
+let userName = "Suman";  // Valid
+let $price = 100;         // Valid
+let _temp = 0;            // Valid
+let name = "Ajay";    // Invalid
---
+++ 02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // since we are using NodeJS, not browser
+
+console.log(3+3);
+
+console.log("Dhanush");
+
+let name = "Kunu"
+let age = 3
+let isLoggedIn = false
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value (empty value representation)
+// undefined => (value not defined)
+// symbol => unique
+
+// object
+console.log(typeof "kunu")
+console.log(typeof age)
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object

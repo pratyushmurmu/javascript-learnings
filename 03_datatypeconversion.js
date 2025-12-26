52cb12a98a4f200aa288bd4c30257d63f8839364
pratyush <pratyushmurmu2004@gmail.com>
2025-12-26 20:44:43 +0530

    Datatype conversion examples Added

---
+++ 03_datatypeconversion.js
@@ -0,0 +1,35 @@
+//let score= "56ab&"
+//let score= null
+//let score= undefined
+//let score=true
+//let score="kunu"
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInnumber= Number(score)
+//console.log(typeof valueInnumber);
+//console.log(valueInnumber);//NaN:Not a Number
+
+// "56" => 56
+// "56ab&" => NaN
+// "null" => 0
+// "undefined" => NaN
+// "true" => 1; "false" => 0
+// "kunu" => NaN
+
+//let isLogggedIn= 1
+//let isLoggedIn = 23
+//let isLoggedIn = 0//false
+//let isLoggedIn = e  //error
+//let isLoggedIn = 0.1//true
+//let isLoggedIn = ""//false
+//let isLoggedIn = "kunu"//true
+
+//let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+
+let anyNumber= 80
+let stringAnyNumber= String(anyNumber)
+console.log(stringAnyNumber);//80
+console.log(typeof stringAnyNumber);//string
+

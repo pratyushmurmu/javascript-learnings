bad8e4f21f00610bfd1f2c23651e232affed6a64
pratyush <pratyushmurmu2004@gmail.com>
2025-12-27 13:51:26 +0530

    Add Datatype operations examples in JS

---
+++ 03.1_operations.js
@@ -0,0 +1,73 @@
+let value = 3
+let negValue = -value
+//console.log(negValue);
+//console.log(2+3)
+//console.log(4*5);
+//console.log(4-4)
+//console.log(5/2);
+//console.log(3%7);
+
+let sent1 = "surprise"
+let sent2 = "mf"
+//console.log(sent1+ +sent2);// surpriseNaN
+
+let no1=2
+let no2=4
+let sum= no1+no2;
+//console.log(sum);
+//console.log(1+2+3);
+//console.log("1"+2+"3");
+//console.log(1+"2"+3);
+//console.log("1"+"2"+3)
+//console.log(1+2+"4");
+console.log(1+(4*5)/2%1);
+
+console.log(true);
+console.log(+true);
+//console.log(true+);//error
+console.log(-true);
+console.log(+false);//0
+console.log(-false);//-0
+
+//console.log(true-)//error
+console.log(+"");//0
+
+let num1, num2, num3
+
+num1=num2=num3=2*5  // not practical in real world coding
+console.log(num1+num2+num3);
+
+let gameCounter = 100
+//gameCounter++;//101
+++gameCounter;//101
+console.log(gameCounter);
+
+//If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+
+
+//If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+
+
+
+
+
+
+
+
+
+
+

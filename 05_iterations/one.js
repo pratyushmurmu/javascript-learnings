b063ce564d8b913bfe94d7715b5e71604ce92329
pratyush <pratyushmurmu2004@gmail.com>
2026-01-08 20:53:00 +0530

    Learned about loops in JS

---
+++ 05_iterations/one.js
@@ -0,0 +1,18 @@
+// for loop
+
+//for (let in//dex = 0; index < array//.length; index++) {
+  //  const eleme//nt// = array[index];//
+    //
+//}
+
+// Syntax of for loop
+
+for(let i = 0; i <= 10; i++){
+    console.log(i); // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    
+}
+
+
+
+
+
